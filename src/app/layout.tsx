283b907e18db913fde965b36f23cd1896fce00fb
Denys Pupin <denyspupin.dev@gmail.com>
2025-01-08 22:42:04 +0100

    Switch to GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID!} />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GAID!} />
         <main className="px-6 md:px-12 lg:px-16 xl:px-32">{children}</main>
       </body>
     </html>

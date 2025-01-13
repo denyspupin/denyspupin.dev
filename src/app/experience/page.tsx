7af33546cdc2f6c5dfc1b0861de195cb9330d9ab
Denys Pupin <denyspupin.dev@gmail.com>
2025-01-13 16:09:53 +0100

    More refacroting

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,6 +1,7 @@
+import JobPosition from "@/components/JobPosition";
 import { experience } from "@/data/experience";
 import getMetadataByPage from "@/helpers/getMetadataByPage";
-import * as motion from "motion/react-client";
+import { Job } from "@/types/experience";
 import { Metadata } from "next";
 
 export const metadata: Metadata = getMetadataByPage("experience");
@@ -11,23 +12,8 @@ const Experience: React.FC = () => {
         Experience
       </h1>
       <ul className="timeline mt-10 flex flex-col gap-y-8 py-10 pl-8 md:mt-10 md:gap-y-10 md:py-16 md:pl-16 lg:mt-16 lg:gap-y-20 lg:py-20 lg:pl-24 xl:max-w-[80%] 2xl:max-w-[55%]">
-        {experience.jobs.map((job) => (
-          <motion.li key={job.id} className="flex flex-col gap-y-5 lg:gap-y-7">
-            <div>
-              <h3 className="timeline-achor relative text-lg font-medium text-accent md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
-                {job.role}
-              </h3>
-              <h4 className="font-light xl:text-lg 2xl:text-xl">
-                {job.period}
-              </h4>
-            </div>
-            <p className="lg:text-lg 2xl:text-xl">{job.companyDescription}</p>
-            <p className="lg:text-lg 2xl:text-xl">{job.roleDescription}</p>
-            <p className="lg:text-lg 2xl:text-xl">
-              <span className="text-accent">Skills: </span>
-              {job.skills.join(", ").toString()}
-            </p>
-          </motion.li>
+        {experience.jobs.map((job: Job) => (
+          <JobPosition key={job.id} job={job} />
         ))}
       </ul>
     </div>

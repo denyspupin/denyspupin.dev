96e3498eaf40db9d06f0cfa08a384c1425412723
Denys Pupin <denyspupin.dev@gmail.com>
2025-11-21 12:48:27 +0100

    Add glass backdrop effect to Topbar and clean up unused imports

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
 import Navbar from "./Navbar";
-import Image from "next/image";
+// import Image from "next/image";
 
 const Topbar = () => {
   return (
     <nav className="sticky top-0 z-10 h-14 backdrop-blur-md">
+      <div className="glass-backdrop"></div>
       <div className="mx-auto flex h-full max-w-[1100px] items-center justify-between px-8 lg:px-0">
-        <Link href="/">
+        {/* <Link href="/">
           <Image
             height={40}
             width={40}
@@ -15,7 +16,8 @@ const Topbar = () => {
             className="rounded-lg"
             alt="Photo of Denys Pupin"
           />
-        </Link>
+        </Link> */}
+        <span className="text-xl font-bold">DP.</span>
         <Navbar />
       </div>
     </nav>
